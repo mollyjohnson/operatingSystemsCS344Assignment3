55c80f52108a3b3a1185f48cf942c230f0e7ef2c
Molly Johnson <johnsmol@os1.engr.oregonstate.edu>
2019-02-22 04:32:14 -0800

    with input/output file redirect for foreground working

--- smallsh.c
+++ smallsh.c
@@ -605,6 +605,7 @@ int main(){
 	//get user input as long as the user hasn't entered "exit"
 	char command[MAX_CHARS];
 	do{
+		int sourceFD, targetFD, result;
 		char inputFile[MAX_CHARS];
 		memset(inputFile, '\0', sizeof(inputFile));
 		strcpy(inputFile, NO_ACTION);
@@ -686,11 +687,27 @@ int main(){
 							printf("child (%d): converting into \'ls -a\'\n", getpid()); fflush(stdout);
 							if(StringMatch(inputFile, NO_ACTION) == FALSE){
 								printf("foreground input file is gonna be redirected!\n");fflush(stdout);
-								RedirectInputFile(inputFile);
+								sourceFD = open(inputFile, O_RDONLY);
+								if(sourceFD == -1){
+									perror("source open() error\n"); exit(1);
+								}
+								printf("sourceFD = %d\n", sourceFD);
+								result = dup2(sourceFD, 0);
+								if(result == -1){
+									perror("source dup2() error\n"); exit(1);
+								}
 							}
 							if(StringMatch(outputFile, NO_ACTION) == FALSE){
 								printf("foreground output file is gonna be redirected!\n"); fflush(stdout);
-								RedirectOutputFile(outputFile);
+								targetFD = open(outputFile, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+								if(targetFD == -1){
+									perror("target open() error\n"); exit(1);
+								}
+								printf("targetFD = %d\n", targetFD);
+								result = dup2(targetFD, 1);
+								if(result == -1){
+									perror("target dup2() error\n"); exit(1);
+								}
 							}
 							Execute(parsedUserInput, &childExitStatus);
 							break;
